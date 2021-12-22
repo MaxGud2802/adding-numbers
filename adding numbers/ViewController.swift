f5fbd55b2d6dac2c881006ae701ded82f6788b90
Maximus Gudino <m_gudino@swbell.net>
2021-12-21 23:20:05 -0600

    Initial Commit

---
+++ adding numbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  adding numbers
+//
+//  Created by Maximus Gudino on 12/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ adding numbersUITests/adding_numbersUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  adding_numbersUITestsLaunchTests.swift
+//  adding numbersUITests
+//
+//  Created by Maximus Gudino on 12/21/21.
+//
+
+import XCTest
+
+class adding_numbersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
