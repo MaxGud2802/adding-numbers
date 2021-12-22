d227a2a685c3eed7782c0d409e5156b772a110fa
Maximus Gudino <m_gudino@swbell.net>
2021-12-22 14:21:21 -0600

    idk how, but the build succeeded at this point in the coding. i am going to try a few ideas branching from this point in the coding process

--- adding numbers/ViewController.swift
+++ adding numbers/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var index = 0
+    @IBOutlet weak var viewHolder: UIView!
+    @IBOutlet var viewBackground: UIView!
+    @IBOutlet weak var labelMathOutput: UILabel!
+    @IBOutlet weak var labelWowMath: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelMathOutput.text = String(index)
+        
     }
-
-
+    @IBAction func buttonMinusOne(_ sender: Any) {
+        if index == index {
+            index+1
+        } else {
+            index+1
+        }
+        
+    }
+    @IBAction func buttonPlusOne(_ sender: Any) {
+        if index == index {
+            index-1
+        } else {
+            index-1
+        }
+    }
 }
 
